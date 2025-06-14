a8fbe5ad4f2cfcbd8b76610815d5353b4d45f133
Adem <ado.designs@outlook.de>
2025-06-15 01:01:38 +0200

    primitive types

--- typescript_project/src/app/app.component.ts
+++ typescript_project/src/app/app.component.ts
@@ -8,5 +8,16 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'typescript_project';
+  // primitive typen
+  String = "das ist ein String"; // zeichenkette
+  number = 42; // eine Zahl
+  boolean = true; // wahr oder falsch
+
+  undefined = undefined; // eine Variable, die nicht definiert ist
+  null = null; // eine variable, die explizit auf "nichts" gesetzt ist
+
+  // erweiterte primitive typen
+  bigint = BigInt(1234567890123456789012345678901234567890); // eine große Zahl, die größer ist als Number.MAX_SAFE_INTEGER
+  symbol = Symbol('ein Symbol'); // ein eindeutiger Wert, der nicht mit anderen Werten verglichen werden kann
+  any = "ich kann alles sein"; // eine Variable, die jeden Typ annehmen kann
 }
