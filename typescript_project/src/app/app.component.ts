7df749164c3ee5c9f255ea2e4c104eeb5e6d23ee
Adem <ado.designs@outlook.de>
2025-06-15 23:06:20 +0200

    feat(types): add object types

--- typescript_project/src/app/app.component.ts
+++ typescript_project/src/app/app.component.ts
@@ -8,34 +8,20 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'Typesccript';
-//tupels
-//   noteType: 200 | 404 = 404;
+  title = "typescript";
+  // types bei Objekten
+  person:{
+    readonly name: string | number; //
+    age: number;
+    position?: string; // ? = nicht zwingend notwendig
+  } = {
+    name: "Adem",
+    age: 27,
+    // position: "Software Engineer"
+  }
 
-//   arr: [string, number] = ["200", 404];
-//   constructor() {
-//     this.arr = ["402", 404];
-// }
+  constructor() {
+    this.person.name =  27;
+
+  }
 }
-
-//#region comments
-// literals
-// noteType: 200 | 404 = 400;
-  
-
-//   arr: [string, 200 | 404][] = []
-
-//   constructor() {
-//     this.noteType = "note";
-
-//     this.handleNoteType();
-//   }
-
-//   handleNoteType() {
-//     if (this.noteType === "note") {
-//       console.log("Notiz wird angezeigt.");
-//     } else if (this.noteType === "trash") {
-//       console.log("Notiz befindet sich im Papierkorb.");
-//     }
-//   }
-//#endregion
