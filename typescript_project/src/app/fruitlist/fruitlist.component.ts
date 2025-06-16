affaacee62f5a1732c958e199911683d2d037454
Adem <ado.designs@outlook.de>
2025-06-16 22:42:46 +0200

    docs(template): add practice example for @for and @empty

--- typescript_project/src/app/fruitlist/fruitlist.component.ts
+++ typescript_project/src/app/fruitlist/fruitlist.component.ts
@@ -15,7 +15,7 @@ export class FruitlistComponent {
       description: "Äpfel sind aufgrund ihres hohen Wassergehalts kalorienarm und enthalten nur Spuren von Fett und Eiweiß, dafür aber rund zwei Prozent Ballaststoffe und etwa elf Prozent Kohlenhydrate. Äpfel enthalten auch viele Vitamine und Mineralstoffe und sind daher eine wichtige Quelle für uns - zum Beispiel für Vitamin C.",
       genus: "Kernobstgewächsen innerhalb der Familie der Rosengewächse",
       stars: 2.3,
-      reviews:[{name: "Kevin W.", text: "ist lecker"},{name: "Arne P.", text: "nicht so meins"}],
+      reviews:[],
     },
     {
       name: "Banane",
